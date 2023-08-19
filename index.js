f8b4046ea880d2c1b5aa786b4b5e81f4046239da
IamAbasz <ikponkeedet84@gmail.com>
2023-08-19 18:47:37 +0100

    new commit

--- index.js
+++ index.js
@@ -14,3 +14,23 @@ if (close){
         nav.classList.remove('active');
     })
 }
+
+let MainImage = document.getElementById("main-img");
+let Smallimg = document.getElementsByClassName("small-img");
+
+Smallimg[0].onclick = function(){
+    MainImage.src = Smallimg[0].src;
+}
+
+Smallimg[1].onclick = function(){
+    MainImage.src = Smallimg[1].src;
+}
+
+Smallimg[2].onclick = function(){
+    MainImage.src = Smallimg[2].src;
+}
+
+Smallimg[3].onclick = function(){
+    MainImage.src = Smallimg[3].src;
+}
+
